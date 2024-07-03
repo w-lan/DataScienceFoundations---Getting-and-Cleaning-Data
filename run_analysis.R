2aa1f59c08405e4298fb47af302a3d4f32970613
william landers <37142465+w-lan@users.noreply.github.com>
2024-07-04 06:15:38 +1000

    complete run_analysis code

--- run_analysis.R
+++ run_analysis.R
@@ -31,7 +31,7 @@ idx_retain_features <- grep("(mean|std)\\(\\)", features[, feature])
 measurements <- features[idx_retain_features, feature]
 measurements <- gsub('[()]', '', measurements)
 
-# Link training and test data with subject ID and activity class
+# Bind training and test data with subject and activity ID
 train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, idx_retain_features, with = FALSE]
 data.table::setnames(train, colnames(train), measurements)
 
@@ -39,4 +39,27 @@ train_activities <- fread(file.path(path, "UCI HAR Dataset/train/y_train.txt"), 
 train_subject <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt"), col.names=c('subject_id'))
 train <- cbind(train_subject, train_activities, train)
 
+test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, idx_retain_features, with = FALSE]
+data.table::setnames(test, colnames(test), measurements)
 
+test_activities <- fread(file.path(path, "UCI HAR Dataset/test/y_test.txt"), col.names=c('activity_id'))
+test_subject <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt"), col.names=c('subject_id'))
+test <- cbind(test_subject, test_activities, test)
+
+combined <- rbind(train, test)
+
+# Convert activity_id to activity label
+combined[["activity_id"]] <- factor(combined[, activity_id]
+                                    ,levels = activity_labels[["activity_id"]]
+                                    ,labels = activity_labels[["label"]])
+setnames(combined, old = "activity_id", new = "activity")
+combined[["subject_id"]] <- as.factor(combined[,subject_id])
+
+# wide > long
+combined <- reshape2::melt(data = combined, id = c("subject_id", "activity_id"))
+
+# Average of each variable for (subject, activity) pairs. 
+combined <- reshape2::dcast(data = combined, subject_id + activity ~ variable, fun.aggregate = mean)
+
+# export results
+data.table::fwrite(x = combined, file = "tidy_data.txt", quote = FALSE)
